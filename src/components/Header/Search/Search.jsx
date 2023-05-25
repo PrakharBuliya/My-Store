f3ec5fad90a41d34507de8ba248c3c205eecc394
Prakhar Buliya <prakharbuliya02@gmail.com>
2023-05-25 14:55:37 +0530

    Added Search Functionality

--- src/components/Header/Search/Search.jsx
+++ src/components/Header/Search/Search.jsx
@@ -3,10 +3,23 @@ import { MdClose } from "react-icons/md";
 import prod from "../../../assets/products/earbuds-prod-1.webp";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import useFetch from "../../../hooks/usefetch";
 
 const Search = ({ setShowSearch }) => {
   const [query, setQuery] = useState();
   const navigate = useNavigate();
+
+  const onChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  let { data } = useFetch(
+    `/api/products?populate=*&[filters][title][$contains]=${query}`
+  );
+
+  if (!query?.length) {
+    data = null;
+  }
 
   return (
     <div className="search-modal">
@@ -16,6 +29,7 @@ const Search = ({ setShowSearch }) => {
           autoFocus
           placeholder="Search for Products"
           value={query}
+          onChange={onChange}
         />
 
         <MdClose className="close-btn" onClick={() => setShowSearch(false)} />
@@ -23,15 +37,30 @@ const Search = ({ setShowSearch }) => {
 
       <div className="search-result-content">
         <div className="search-results">
-          <div className="search-result-item">
-            <div className="img-container">
-              <img src={prod} alt="" />
+          {data?.data?.map((item) => (
+            <div
+              key={item.id}
+              className="search-result-item"
+              onClick={() => {
+                navigate("/product/" + item.id);
+                setShowSearch(false);
+              }}
+            >
+              <div className="img-container">
+                <img
+                  src={
+                    process.env.REACT_APP_DEV_URL +
+                    item.attributes.img.data[0].attributes.url
+                  }
+                  alt=""
+                />
+              </div>
+              <div className="prod-details">
+                <span className="name">{item.attributes.title}</span>
+                <span className="desc">{item.attributes.desc}</span>
+              </div>
             </div>
-            <div className="prod-details">
-              <span className="name">product name</span>
-              <span className="desc">product desc</span>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
